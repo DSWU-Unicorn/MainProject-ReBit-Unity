2cfdc7b471ef5deeb986d54389f1ca1859e921cc
allzeroyou <0fcoursewhynot@duksung.ac.kr>
2022-12-16 00:13:00 +0900

    Feat: 건강신호등, 레벨업

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,17 +8,18 @@ public class UIManager : MonoBehaviour
     // public DataController dataController;
     public Text goldPerClickDisplayer;
     public Text goldPerSecDisplayer;
+    public Text LightDisplayer;
 
     //==
     //== 
-    // public Text light;
+    public Text light;
 
     void Update() {
         // 보통 게임은 초당 30~60번 정도 화면을 갱신함 
         // goldDisplayer.text = "GOLD: " + dataController.GetGold();
-        goldDisplayer.text = "POINT: " + DataController.GetInstance().GetGold();
+        goldDisplayer.text = "" + DataController.GetInstance().GetGold();
         goldPerClickDisplayer.text = "LEVEL: " + DataController.GetInstance().GetLv();
-        goldPerSecDisplayer.text = "EXP: " +  DataController.GetInstance().GetLevel();
-        // goldPerSecDisplayer.text = "EXP: " +  DataController.GetInstance().GetLight();
+        goldPerSecDisplayer.text = "" +  DataController.GetInstance().GetLevel(); //exp
+        LightDisplayer.text = "Light: " +  DataController.GetInstance().GetLight();
     }
 }
